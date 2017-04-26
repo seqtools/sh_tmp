ff68577d8d93b8d665c8e8f5191f3d666c55e7b1
Qian <qian.xie@csra.com>
2017-04-26 14:50:40 -0400

    2

--- refgendownload.sh
+++ refgendownload.sh
@@ -1,6 +1,10 @@
-(echo Step 1 Downloading iGenomes; date) | sed 'N;s/\n/ /'
-(echo Step 2 Downloading iGenomes; date) | sed 'N;s/\n/ /'
-(echo Step 3 Downloading iGenomes; date) | sed 'N;s/\n/ /'
-(echo Step 4 Downloading iGenomes; date) | sed 'N;s/\n/ /'
-(echo Step 5 Downloading iGenomes; date) | sed 'N;s/\n/ /'
+(echo Step 1 iGenomes; date) | sed 'N;s/\n/ /'
+echo
+(echo Step 2 iGenomes; date) | sed 'N;s/\n/ /'
+echo
+(echo Step 3 iGenomes; date) | sed 'N;s/\n/ /'
+echo
+(echo Step 4 iGenomes; date) | sed 'N;s/\n/ /'
+echo
+(echo Step 5 iGenomes; date) | sed 'N;s/\n/ /'
 
